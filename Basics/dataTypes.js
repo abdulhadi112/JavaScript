fd3f9bebb42b304b8518f7efff247f119b93e9fd
AbdulHadi <abdulhadi.bavani@gmail.com>
2024-07-25 23:36:15 +0530

    Learn : different data types

---
+++ Basics/dataTypes.js
@@ -0,0 +1,25 @@
+// LECTURE 2
+let name = "AbdulHadi" //String
+let age = 20 // number
+let isLoggedIn = true // Boolean
+let state //undefined
+
+/*types : number
+bigInt
+string
+boolean
+null(stand alone value & a object)
+undefined(abhi value assign nhi hui hai)
+symbol(for uniqueness)
+object (VVIMP topic)
+*/
+
+// const num 
+// num = 5;
+//You also cannot declare a const-variable without assigning a value to it directly
+// console.log(num)
+
+console.log(Boolean(100)) //we forced the value 100 to be converted to a Boolean, and the result is true.
+//This is because every single value in JavaScript is considered either truthy or falsy. Truthy values result in the value true when coerced into a Boolean value. Falsy values result in the value false.
+//Values considered falsy are : false, null, undefined, 0, -0, NaN & empty string
+
