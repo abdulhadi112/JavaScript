b3c171e0e53084a1e5f72d462b7d29c2b204f4ff
AbdulHadi <abdulhadi.bavani@gmail.com>
2024-07-25 19:29:31 +0530

    add notes to varConst.js

--- Basics/varConst.js
+++ Basics/varConst.js
@@ -1,2 +1,14 @@
 const accountId = 145789
-let accountName = "AbdulHadi"
+let accountEmail = "abdulhadi@google.com"
+var accountPassword = "1234"
+accountCity = "Mumbai"
+let accountState // When a variable is declared but not assigned any value, it returns 'undefined'
+
+// accountId = 2 // not allowed to change value of const variable
+
+accountEmail = "abdulhadi@chaicode.com"
+accountPassword = "12121"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
