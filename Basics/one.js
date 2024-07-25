756d0c383e85333aea9be04c87df9115c8dd37c4
AbdulHadi <abdulhadi.bavani@gmail.com>
2024-07-25 18:19:10 +0530

    documented git learnings

--- Basics/one.js
+++ Basics/one.js
@@ -1,1 +1,14 @@
 console.log("Hello JS world");
+/*
+Humesha root folder mein 'git init' command run karna. In this case JavaScript is the root folder. jaha par tumhare saare folder hoge  jaise ki 'advanced' 'promises' etc
+
+agar kisi file mein changes ki hai tabh bhi root folder se hi git use karna!! IMP
+EG :
+CHANGES MADE IN : JavaScript/Basics/varConst.js 
+GO TO :Desktop/JavaScript (i.e root folder) RUN COMMAND : git status 
+(WILL RETURN THAT CHANGES WHERE MADE IN) -> Basics/varConst.js
+THEN : 
+git add .
+git commit -m "..."
+git push origin master
+*/
