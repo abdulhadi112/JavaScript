fc6675ec735bd617f81c2d2ca879cf2f67561fa8
AbdulHadi <abdulhadi.bavani@gmail.com>
2024-08-01 21:45:55 +0530

    Learn : Dates, number & Maths

---
+++ Basics/dates.js
@@ -0,0 +1,8 @@
+//Lecture 7
+// Dates 
+
+let myDate = new Date() // Created a object of Date
+// console.log(myDate); //Not readable output
+// console.log(myDate.toString());//Mon Jul 29 2024 17:28:49 GMT+0530 (India Standard Time)
+//console.log(myDate.toDateString())// Returns just (Mon Jul 29 2024)
+console.log(myDate.toLocaleString()) // Returns dd/mm/yyyy & time in 12-hour format
